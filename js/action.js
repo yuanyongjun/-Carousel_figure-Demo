91e9da796451fcba23ab71ca7b69cf3b8bad3f72
yuanyongjun <1195029042@qq.com>
2017-07-20 12:03:20 +0800

    修改bug

--- js/action.js
+++ js/action.js
@@ -13,14 +13,16 @@ window.onload = function () {
 	function animate(offset) {
 		//获取的是style.left，是相对左边获取距离，所以第一张图后style.left都为负值，
 		//且style.left获取的是字符串，需要用parseInt()取整转化为数字。
-		var newLeft = parseInt(list.style.left) + offset;
+		//var left=
+		var left=document.defaultView.getComputedStyle(list,null).left;
+		var newLeft = parseInt(left) + offset;
 		list.style.left = newLeft + 'px';
 		//无限滚动判断
-		if (newLeft > -600) {
-			list.style.left = -3000 + 'px';
+		if (newLeft > 0) {
+			list.style.left = -3500 + 'px';
 		}
-		if (newLeft < -3000) {
-			list.style.left = -600 + 'px';
+		if (newLeft < -3500) {
+			list.style.left = 0 + 'px';
 		}
 	}
 
@@ -52,19 +54,19 @@ window.onload = function () {
 	prev.onclick = function () {
 		index -= 1;
 		if (index < 1) {
-			index = 5
+			index = 6
 		}
 		buttonsShow();
-		animate(600);
+		animate(700);
 	};
 
 	next.onclick = function () {
 		//由于上边定时器的作用，index会一直递增下去，我们只有5个小圆点，所以需要做出判断
 		index += 1;
-		if (index > 5) {
+		if (index > 6) {
 			index = 1
 		}
-		animate(-600);
+		animate(-700);
 		buttonsShow();
 	};
 
@@ -74,7 +76,7 @@ window.onload = function () {
 				/*  这里获得鼠标移动到小圆点的位置，用this把index绑定到对象buttons[i]上，去谷歌this的用法  */
 				/*  由于这里的index是自定义属性，需要用到getAttribute()这个DOM2级方法，去获取自定义index的属性*/
 				var clickIndex = parseInt(this.getAttribute('index'));
-				var offset = 600 * (index - clickIndex); //这个index是当前图片停留时的index
+				var offset = 700 * (index - clickIndex); //这个index是当前图片停留时的index
 				animate(offset);
 				index = clickIndex; //存放鼠标点击后的位置，用于小圆点的正常显示
 				buttonsShow();
